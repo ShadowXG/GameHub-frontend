7342055b5593be55bbd2bd8b1aeb7617757a5bb2
danieljo <hyundanieljo@gmail.com>
2023-02-18 20:43:44 -0500

    changes made to gameindex, showgame, and home.js

--- src/components/Games/GamesIndex.js
+++ src/components/Games/GamesIndex.js
@@ -72,7 +72,7 @@ const GamesIndex = (props) => {
                                 minWidth: '295px', 
                                 minHeight: '170px', 
                                 maxHeight: '170px', 
-                                border: '5px solid #B0C4DE',
+                                border: '2px solid #B0C4DE',
                                 borderRadius: '10px',
                                 marginBottom: '15px',
                                 }} 
@@ -101,7 +101,7 @@ const GamesIndex = (props) => {
                                 minWidth: '295px', 
                                 minHeight: '170px', 
                                 maxHeight: '170px', 
-                                border: '5px solid #B0C4DE',
+                                border: '2px solid #B0C4DE',
                                 borderRadius: '10px',
                                 marginBottom: '15px',
                                 }} 
